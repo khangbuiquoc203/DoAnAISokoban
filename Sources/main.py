7ab9cc1a9c7d85bdcb3f6750e9e39e0791f8fa6b
khangbuiquoc203 <khangbuiquoc203@gmail.com>
2023-11-27 12:24:05 +0700

    BACKUP FILE LOGIN

--- Sources/main.py
+++ Sources/main.py
@@ -1,7 +1,6 @@
-#import login
+import login
 import pygame
 import const as c
 import build_map as b
-screen = pygame.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT)) 
-b.sokoban(screen, 0)
-#login.init()
+
+login.init()
