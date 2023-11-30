a912a95605eaf5704b71883af0ed8726d3eef047
hungphong <hungphong.congviec@gmail.com>
2023-11-30 22:35:41 +0700

    setting

---
+++ Sources/setting.py
@@ -0,0 +1,15 @@
+import pygame
+
+up_player1 = pygame.K_UP
+down_player1 = pygame.K_DOWN
+left_player1 = pygame.K_LEFT
+right_player1 = pygame.K_RIGHT
+undo_player1 = pygame.K_BACKSPACE
+
+up_player2 = pygame.K_w
+down_player2 = pygame.K_s
+left_player2 = pygame.K_a
+right_player2 = pygame.K_d
+undo_player2 = pygame.K_ESCAPE
+
+reset = pygame.K_SPACE
