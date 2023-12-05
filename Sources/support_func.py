f0e6b4bb09bf3b651a9c6e4c709a5975b2768005
Hao <93723433+Hao12B2@users.noreply.github.com>
2023-12-05 11:33:56 +0700

    Update support_func.py

--- Sources/support_func.py
+++ Sources/support_func.py
@@ -30,16 +30,6 @@ class state:
             current_state = current_state.state_parent
         path.reverse()
         return path
-            
-            
-    
-    ''' COMPUTE HEURISTIC FUNCTION USED FOR GREEDY, A* ALGORITHM
-    def h(self):
-        list_boxes = find_boxes_position(self.board)
-        if self.heuristic == 0:
-            self.heuristic = abs(sum(list_boxes[i][0] + list_boxes[i][1] - self.check_points[i][0] - self.check_points[i][1] for i in range(len(list_boxes))))
-        return self.heuristic
-    '''
     
     def h(self):
         list_boxes = find_boxes_position(self.board) 
@@ -98,7 +88,6 @@ def is_box_on_check_point(box, list_check_point):
 ''' CHECK WHETHER A SIGNLE BOX IS STUCK IN THE CORNER '''
 def check_in_corner(board, x, y, list_check_point):
     '''return true if board[x][y] in corner'''
-
     if (
         (board[x-1][y-1] == '#' and board[x-1][y] == '#' and board[x][y-1] == '#') or
         (board[x+1][y-1] == '#' and board[x+1][y] == '#' and board[x][y-1] == '#') or
@@ -152,91 +141,50 @@ def find_boxes_position(board):
 
 ''' CHECK WHETHER A SINGLE BOX CAN BE MOVED IN AT LEAST 1 DIRECTION'''
 def is_box_can_be_moved(board, box_position):
-    left_move = (box_position[0], box_position[1] - 1) 
-    right_move = (box_position[0], box_position[1] + 1)
-    up_move = (box_position[0] - 1, box_position[1])
-    down_move = (box_position[0] + 1, box_position[1])
-    if (board[left_move[0]][left_move[1]] == ' ' or board[left_move[0]][left_move[1]] == '%' or board[left_move[0]][left_move[1]] == '@') and board[right_move[0]][right_move[1]] != '#' and board[right_move[0]][right_move[1]] != '$':
-        return True
-    if (board[right_move[0]][right_move[1]] == ' ' or board[right_move[0]][right_move[1]] == '%' or board[right_move[0]][right_move[1]] == '@') and board[left_move[0]][left_move[1]] != '#' and board[left_move[0]][left_move[1]] != '$':
-        return True
-    if (board[up_move[0]][up_move[1]] == ' ' or board[up_move[0]][up_move[1]] == '%' or board[up_move[0]][up_move[1]] == '@') and board[down_move[0]][down_move[1]] != '#' and board[down_move[0]][down_move[1]] != '$':
-        return True
-    if (board[down_move[0]][down_move[1]] == ' ' or board[down_move[0]][down_move[1]] == '%' or board[down_move[0]][down_move[1]] == '@') and board[up_move[0]][up_move[1]] != '#' and board[up_move[0]][up_move[1]] != '$':
-        return True
-    return False
+    board = np.array(board)
+    possible_moves = np.array([(box_position[0], box_position[1] + i) for i in (-1, 1)] + [(box_position[0] + i, box_position[1]) for i in (-1, 1)])
+    return np.any(np.isin(board[possible_moves[:, 0], possible_moves[:, 1]], (' ', '%', '@')) & ~np.isin(board[box_position[0] + possible_moves[:, 0] - box_position[0], box_position[1] + possible_moves[:, 1] - box_position[1]], ('#', '$')))
 
-
-''' GET THE NEXT POSSIBLE MOVE '''
 def get_next_pos(board, cur_pos):
-    '''return list of positions that player can move to from current position'''
-    x,y = cur_pos[0], cur_pos[1]
+    board = np.array(board)
+    x, y = cur_pos
     list_can_move = []
-    # MOVE UP (x - 1, y)
-    if 0 <= x - 1 < len(board):
-        value = board[x - 1][y]
-        if value == ' ' or value == '%':
-            list_can_move.append((x - 1, y))
-        elif value == '$' and 0 <= x - 2 < len(board):
-            next_pos_box = board[x - 2][y]
-            if next_pos_box != '#' and next_pos_box != '$':
-                list_can_move.append((x - 1, y))
-    # MOVE DOWN (x + 1, y)
-    if 0 <= x + 1 < len(board):
-        value = board[x + 1][y]
-        if value == ' ' or value == '%':
-            list_can_move.append((x + 1, y))
-        elif value == '$' and 0 <= x + 2 < len(board):
-            next_pos_box = board[x + 2][y]
-            if next_pos_box != '#' and next_pos_box != '$':
-                list_can_move.append((x + 1, y))
-    # MOVE LEFT (x, y - 1)
-    if 0 <= y - 1 < len(board[0]):
-        value = board[x][y - 1]
-        if value == ' ' or value == '%':
-            list_can_move.append((x, y - 1))
-        elif value == '$' and 0 <= y - 2 < len(board[0]):
-            next_pos_box = board[x][y - 2]
-            if next_pos_box != '#' and next_pos_box != '$':
-                list_can_move.append((x, y - 1))
-    # MOVE RIGHT (x, y + 1)
-    if 0 <= y + 1 < len(board[0]):
-        value = board[x][y + 1]
-        if value == ' ' or value == '%':
-            list_can_move.append((x, y + 1))
-        elif value == '$' and 0 <= y + 2 < len(board[0]):
-            next_pos_box = board[x][y + 2]
-            if next_pos_box != '#' and next_pos_box != '$':
-                list_can_move.append((x, y + 1))
-    return list_can_move
 
+    # Define the possible moves
+    moves = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    for dx, dy in moves:
+        new_x, new_y = x + dx, y + dy
+
+        if 0 <= new_x < len(board) and 0 <= new_y < len(board[0]):
+            value = board[new_x, new_y]
+
+            if value == ' ' or value == '%':
+                list_can_move.append((new_x, new_y))
+            elif value == '$' and 0 <= new_x + dx < len(board) and 0 <= new_y + dy < len(board[0]):
+                next_pos_box = board[new_x + dx, new_y + dy]
+
+                if next_pos_box != '#' and next_pos_box != '$':
+                    list_can_move.append((new_x, new_y))
+
+    return list_can_move
 
 ''' MOVE THE BOARD IN CERTAIN DIRECTIONS '''
 def move(board, next_pos, cur_pos, list_check_point):
-    '''return a new board after move'''
-    new_board = deepcopy(board) 
-    # FIND NEXT POSITION IF MOVE TO BOX
-    if new_board[next_pos[0]][next_pos[1]] == '$':
-        x = 2*next_pos[0] - cur_pos[0]
-        y = 2*next_pos[1] - cur_pos[1]
-        new_board[x][y] = '$'
-    # MOVE PLAYER TO NEW POSITION
-    new_board[next_pos[0]][next_pos[1]] = '@'
-    new_board[cur_pos[0]][cur_pos[1]] = ' '
-    # CHECK IF AT CHECK POINT'S POSITION DON'T HAVE ANYTHING THEN UPDATE % LIKE CHECK POINT
-    for p in list_check_point:
-        if new_board[p[0]][p[1]] == ' ':
-            new_board[p[0]][p[1]] = '%'
-    return new_board
+    new_board = np.array(board)
+    if new_board[next_pos] == '$':
+        opposite_pos = tuple(2 * np.array(next_pos) - np.array(cur_pos))
+        new_board[opposite_pos] = '$'
+    new_board[next_pos] = '@'
+    new_board[cur_pos] = ' '
 
+    check_chars = new_board[tuple(list_check_point.T)]
+    free_indices = np.where(check_chars == ' ')
+    new_board[tuple(list_check_point.T[:, free_indices])] = '%'
+    return new_board.tolist()
 
 ''' FIND ALL CHECKPOINTS ON THE BOARD '''
 def find_list_check_point(board):
-    '''return list check point form the board
-        if don't have any check point, return empty list
-        it will check num of box, if num of box < num of check point
-            return list [(-1,-1)]'''
-    #list_check_point = []
     num_of_box = 0
     array = np.array(board)
     num_of_box = np.count_nonzero(array=='$')
@@ -249,17 +197,6 @@ def find_list_check_point(board):
     result = xy.tolist()
     return result
 
-    ''' CHECK THE ENTIRE BOARD TO FIND CHECK POINT AND NUM OF BOX
-    for x in range(len(board)):
-        for y in range(len(board[0])):
-            if board[x][y] == '$':
-                num_of_box += 1
-            elif board[x][y] == '%':
-                list_check_point.append((x,y))
-    if num_of_box < len(list_check_point):
-        return [(-1,-1)]
-    return list_check_point
-    '''
     
 '''FUNCTION RETURN NEW BOARD AFTER MOVE IN 1 DIRECTION'''
 def move_in_1_direction(board, direct, list_check_point):
